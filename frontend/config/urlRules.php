47c8777a33498c3601cf456d006e0760760e9d9e
Saw <lawdrel@gmail.com>
2018-05-25 17:14:47 +0300

    al

---
+++ frontend/config/urlRules.php
@@ -0,0 +1,44 @@
+<?php
+return [
+//    '/' => 'site/index',
+//    [
+//        'pattern' => 'news',
+//        'route' => 'news/default/index',
+//        'suffix' => '.html',
+//    ],
+//    [
+//        'pattern' => '<action>',
+//        'route' => 'site/<action>',
+//        'suffix' => '.html',
+//    ],
+//    [
+//        'pattern' => '<module>/<id:\d+>',
+//        'route' => '<module>/default/view',
+//        'suffix' => '.html',
+//    ],
+
+
+
+
+    '/' => 'site/index',
+    'modal' => 'lawsaw/modal',
+
+//    '' => 'site/index',
+//    '<controller:\w+>/<action:\w+>/' => '<controller>/<action>',
+
+
+    [
+        'pattern' => 'about',
+        'route' => 'site/about',
+        'suffix' => '/',
+    ],
+    [
+        'pattern' => 'modal',
+        'route' => 'modal/index',
+        'suffix' => '.html',
+    ],
+
+
+
+
+];
