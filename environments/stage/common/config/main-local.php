44f04fc7874961bfc5adfefb9530ec209234901a
Saw <lawdrel@gmail.com>
2018-05-28 23:57:02 +0300

    gdfgd

--- environments/stage/common/config/main-local.php
+++ environments/stage/common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=saw04_yii2adv',
+            'dsn' => 'mysql:host=saw04.mysql.tools;dbname=saw04_yii2adv',
             'username' => 'saw04_yii2adv',
             'password' => 'b5awjdez',
             'charset' => 'utf8',
