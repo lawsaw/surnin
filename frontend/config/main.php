ec75a55f6ce40ddbe6c7ba68f8675ee491390851
Saw <lawdrel@gmail.com>
2018-05-29 15:09:36 +0300

    fdsgfdsf

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -43,7 +43,7 @@ return [
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-            'baseUrl' => '',
+            'baseUrl' => '/',
             'class' => \lawsaw\components\LangUrlManager::className(),
             'rules' => require __DIR__."/urlRules.php",
             'normalizer' => [
